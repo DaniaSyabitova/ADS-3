c6bda0161126f3cb1cf9d27ef0064e4ef0c7622c
Dania Syabitova <94595600+DaniaSyabitova@users.noreply.github.com>
2022-03-11 20:03:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int counter = 0;
+  int i = 0, j = size - 1;
+  while (i < j) {
+    int mid = i + (j - 1) / 2;
+    if (arr[mid] == value) {
+      counter++;
+      while (arr[mid + 1] == value) {
+        counter++;
+        mid++;
+      }
+      mid = i + (j - 1) / 2;
+      while (arr[mid - 1] == value) {
+        counter++;
+        mid--;
+      }
+      break;
+    }
+    else if (arr[mid] > value)
+      j = mid;
+    else
+      i = mid + 1;
+      }
+  if (counter != 0)
+    return counter;
   return 0; // если ничего не найдено
 }
